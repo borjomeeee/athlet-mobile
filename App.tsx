c7dc257c0cb16d1eab7df5045253d31ff747184d
borjomeeee <spiridonov.new@gmail.com>
2022-03-16 14:52:44 +0300

    fix(husky): add 'git add' step

--- App.tsx
+++ App.tsx
@@ -33,24 +33,24 @@ const Section: React.FC<{
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-    <Text
-      style={[
-        styles.sectionTitle,
-        {
-          color: isDarkMode ? Colors.white : Colors.black,
-        },
-      ]}>
-      {title}
-    </Text>
-    <Text
-      style={[
-        styles.sectionDescription,
-        {
-          color: isDarkMode ? Colors.light : Colors.dark,
-        },
-      ]}>
-      {children}
-    </Text>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
     </View>
   );
 };
