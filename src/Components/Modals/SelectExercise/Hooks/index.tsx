99c0c458813a5729a99f9fdc9165fd16de6ea15b
borjomeeee <spiridonov.new@gmail.com>
2022-03-29 12:18:50 +0300

    fix(exercises): add reset on closing select exercise modal

--- src/Components/Modals/SelectExercise/Hooks/index.tsx
+++ src/Components/Modals/SelectExercise/Hooks/index.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import {useSelectExerciseStore} from '../Store';
 
 export const useSelectExerciseController = (id: string) => {
   const {setSearchValue} = useSelectExerciseStore(id);
-  return {handleChangeSearchValue: setSearchValue};
+
+  const reset = React.useCallback(() => {
+    setSearchValue('');
+  }, [setSearchValue]);
+
+  return {handleChangeSearchValue: setSearchValue, reset};
 };
--- src/Components/Modals/SelectExercise/index.tsx
+++ src/Components/Modals/SelectExercise/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Exercise} from 'src/Store/Models/Training';
 import {BottomSheetModal, HSpacer} from '../../Common';
+import {useSelectExerciseController} from './Hooks';
 import {Header} from './Views/Header';
 import {List} from './Views/List';
 
@@ -12,6 +13,9 @@ export const SelectExercise: React.FC<SelectExerciseProps> = ({
   id,
   onSelect,
 }) => {
+  const {reset} = useSelectExerciseController(id);
+  React.useEffect(() => () => reset(), [reset]);
+
   return (
     <BottomSheetModal id={id} snapPoints={['80%']}>
       <Header id={id} />
